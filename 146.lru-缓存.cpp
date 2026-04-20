3b3205e3e6a6dcdbccc1140b29ab1d5d5bf108f7
trv3wood <141489188+trv3wood@users.noreply.github.com>
2026-04-20 23:07:57 +0800

    146

--- 146.lru-缓存.cpp
+++ 146.lru-缓存.cpp
@@ -11,37 +11,41 @@
  * Dislikes: 0
  * Total Accepted:    1.1M
  * Total Submissions: 2M
- * Testcase Example:  '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
+ * Testcase Example:
+ '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
   '[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]'
  *
  * 请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
- * 
+ *
  * 实现 LRUCache 类：
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
- * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
- * void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组
- * key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
- * 
- * 
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1
+ 。
+ * void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value
+ ；如果不存在，则向缓存中插入该组
+ * key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出
+ 最久未使用的关键字。
+ *
+ *
  * 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  * 示例：
- * 
- * 
+ *
+ *
  * 输入
  * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
  * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
  * 输出
  * [null, null, null, 1, null, -1, null, -1, 3, 4]
- * 
+ *
  * 解释
  * LRUCache lRUCache = new LRUCache(2);
  * lRUCache.put(1, 1); // 缓存是 {1=1}
@@ -53,75 +57,56 @@
  * lRUCache.get(1);    // 返回 -1 (未找到)
  * lRUCache.get(3);    // 返回 3
  * lRUCache.get(4);    // 返回 4
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= capacity <= 3000
  * 0 <= key <= 10000
  * 0 <= value <= 10^5
  * 最多调用 2 * 10^5 次 get 和 put
- * 
- * 
+ *
+ *
  */
 
-#include <algorithm>
 #include <cassert>
-#include <map>
+#include <list>
+#include <unordered_map>
 #include <utility>
 
-#include <vector>
 // @lc code=start
 class LRUCache {
 public:
-    std::vector<int> data; // [val]
-    std::map<int, std::pair<int, int>> m; // key, <index, counter>
-    LRUCache(int capacity) {
-        data.reserve(capacity);
+  std::list<std::pair<int, int>> l; // [<key, val>]
+  std::unordered_map<int, std::list<std::pair<int, int>>::iterator> m;
+  int capacity;
+  LRUCache(int capacity) : capacity(capacity) {}
+
+  int get(int key) {
+    if (m.find(key) == m.end()) {
+      return -1;
     }
-    void updateCounter() {
-        for (auto& c : m) {
-            c.second.second >>= 1;
-        }
+    l.splice(l.begin(), l, m[key]);
+    return m[key]->second;
+  }
+
+  void put(int key, int value) {
+    if (m.find(key) != m.end()) {
+      l.splice(l.begin(), l, m[key]);
+      m[key]->second = value;
+      return;
     }
-    
-    int get(int key) {
-        updateCounter();
-        auto it = m.find(key);
-        if (it == m.end()) {
-            return -1;
-        } else {
-            it->second.second |= 1 << data.capacity();
-            return data[it->second.first];
-        }
+    if (l.size() == capacity) {
+      auto &back = l.back();
+      m.erase(back.first);
+      l.pop_back();
     }
-    
-    void put(int key, int value) {
-        updateCounter();
-        
-        if (m.find(key) != m.end()) {
-            data[m[key].first] = value;
-            m[key].second |= 1 << data.capacity();    
-            return;
-        }
-        if (data.size() < data.capacity()) {
-            data.push_back(value);
-            assert(data.size() - 1 >= 0);
-            m.insert({key, {data.size() - 1, 1 << data.capacity()}});
-        } else {
-            auto lru = std::min_element(m.begin(), m.end(), [](auto& l, auto& r) {
-                return l.second.second < r.second.second;
-            });
-            int lru_idx = lru->second.first;
-            assert(0 <= lru_idx && lru_idx < data.size());
-            data[lru_idx] = value;
-            m.erase(lru);
-            m.insert({key, {lru_idx, 1 << data.capacity()}});
-        }
-    }
+    l.emplace_front(key, value);
+    m.emplace(key, l.begin());
+  }
 };
 
 /**
@@ -131,4 +116,3 @@ public:
  * obj->put(key,value);
  */
 // @lc code=end
-
