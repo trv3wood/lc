77ef220bb736c81bb4515967e4af8388680c4020
trv3wood <141489188+trv3wood@users.noreply.github.com>
2026-04-20 19:43:57 +0800

    146 todo

---
+++ 146.lru-缓存.cpp
@@ -0,0 +1,134 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=cpp
+ *
+ * [146] LRU 缓存
+ *
+ * https://leetcode.cn/problems/lru-cache/description/
+ *
+ * algorithms
+ * Medium (55.36%)
+ * Likes:    3798
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 2M
+ * Testcase Example:  '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
+  '[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]'
+ *
+ * 请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
+ * 
+ * 实现 LRUCache 类：
+ * 
+ * 
+ * 
+ * 
+ * LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组
+ * key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+ * 
+ * 
+ * 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 输入
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * 输出
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ * 
+ * 解释
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // 缓存是 {1=1}
+ * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+ * lRUCache.get(1);    // 返回 1
+ * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+ * lRUCache.get(2);    // 返回 -1 (未找到)
+ * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+ * lRUCache.get(1);    // 返回 -1 (未找到)
+ * lRUCache.get(3);    // 返回 3
+ * lRUCache.get(4);    // 返回 4
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= capacity <= 3000
+ * 0 <= key <= 10000
+ * 0 <= value <= 10^5
+ * 最多调用 2 * 10^5 次 get 和 put
+ * 
+ * 
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <map>
+#include <utility>
+
+#include <vector>
+// @lc code=start
+class LRUCache {
+public:
+    std::vector<int> data; // [val]
+    std::map<int, std::pair<int, int>> m; // key, <index, counter>
+    LRUCache(int capacity) {
+        data.reserve(capacity);
+    }
+    void updateCounter() {
+        for (auto& c : m) {
+            c.second.second >>= 1;
+        }
+    }
+    
+    int get(int key) {
+        updateCounter();
+        auto it = m.find(key);
+        if (it == m.end()) {
+            return -1;
+        } else {
+            it->second.second |= 1 << data.capacity();
+            return data[it->second.first];
+        }
+    }
+    
+    void put(int key, int value) {
+        updateCounter();
+        
+        if (m.find(key) != m.end()) {
+            data[m[key].first] = value;
+            m[key].second |= 1 << data.capacity();    
+            return;
+        }
+        if (data.size() < data.capacity()) {
+            data.push_back(value);
+            assert(data.size() - 1 >= 0);
+            m.insert({key, {data.size() - 1, 1 << data.capacity()}});
+        } else {
+            auto lru = std::min_element(m.begin(), m.end(), [](auto& l, auto& r) {
+                return l.second.second < r.second.second;
+            });
+            int lru_idx = lru->second.first;
+            assert(0 <= lru_idx && lru_idx < data.size());
+            data[lru_idx] = value;
+            m.erase(lru);
+            m.insert({key, {lru_idx, 1 << data.capacity()}});
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+// @lc code=end
+
