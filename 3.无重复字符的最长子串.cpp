cddcfdd16752ba34e5b086d94218742a6957a62d
trv3wood <141489188+trv3wood@users.noreply.github.com>
2026-04-20 10:14:07 +0800

    3 perf

--- 3.无重复字符的最长子串.cpp
+++ 3.无重复字符的最长子串.cpp
@@ -54,20 +54,34 @@
  *
  */
 
+#include <array>
 #include <string>
 using namespace std;
 // @lc code=start
 class Solution {
 public:
-  int lengthOfLongestSubstring(string s) { 
-    int count[128]{0};
+  // int lengthOfLongestSubstring(string s) {
+  //   int count[128]{0};
+  //   int ans = 0;
+  //   for (int i = 0, j = 0; j < s.size(); j++) {
+  //     count[s[j]]++;
+  //     while (count[s[i]] > 1) {
+  //       count[s[i]]--;
+  //       i++;
+  //     }
+  //     ans = std::max(ans, j - i + 1);
+  //   }
+  //   return ans;
+  // }
+  int lengthOfLongestSubstring(string s) {
+    std::array<int, 128> pos_map; // 记录字符最后出现的位置
+    pos_map.fill(-1);
     int ans = 0;
     for (int i = 0, j = 0; j < s.size(); j++) {
-      count[s[j]]++;
-      while (count[s[i]] > 1) {
-        count[s[i]]--;
-        i++;
+      if (j != pos_map[s[j]] && pos_map[s[j]] >= i) {
+        i = pos_map[s[j]] + 1; // 重复时 i 直接跳跃
       }
+      pos_map[s[j]] = j;
       ans = std::max(ans, j - i + 1);
     }
     return ans;
