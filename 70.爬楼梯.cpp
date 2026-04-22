2e9b3470c2df43748c688c04f502f46ef0660db2
trv3wood <141489188+trv3wood@users.noreply.github.com>
2026-04-22 16:28:06 +0800

    70, 322 动态规划

---
+++ 70.爬楼梯.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=cpp
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode.cn/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (55.64%)
+ * Likes:    4059
+ * Dislikes: 0
+ * Total Accepted:    2.2M
+ * Total Submissions: 3.9M
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 2
+ * 输出：2
+ * 解释：有两种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶
+ * 2. 2 阶
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：3
+ * 解释：有三种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶 + 1 阶
+ * 2. 1 阶 + 2 阶
+ * 3. 2 阶 + 1 阶
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= n <= 45
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int climbStairs(int n) {
+        int pre1 = 0, pre2 = 1, cur = 1;
+        for (int i = 1; i < n; i++) {
+            pre1 = pre2;
+            pre2 = cur;
+            cur = pre1 + pre2;
+        }
+        return cur;
+    }
+};
+// @lc code=end
+
